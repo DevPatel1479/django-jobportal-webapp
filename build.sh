68244f3989aea421388d1eefe19e909e11b92bad
Dev Patel <devpatel1828king@gmail.com>
2025-05-07 22:25:12 +0530

    updated build

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ python manage.py makemigrations
 # Convert static asset files
 python manage.py collectstatic --no-input
 
-python manage.py migrate users
+
 
 # Apply any outstanding database migrations
 python manage.py migrate
